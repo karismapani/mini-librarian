5ba09f7bd64a8a531bbf2e11cb885a323fcc63b0
karismapani <karismapani3@gmail.com>
2026-06-06 10:56:08 +0530

    added database.py

---
+++ database.py
@@ -0,0 +1,7 @@
+from sqlmodel import SQLModel, create_engine, Session  
+engine = create_engine("sqlite:///library.db")
+def create_db():
+    SQLModel.metadata.create_all(engine)  
+def get_session():
+    with Session(engine) as session:
+        yield session
